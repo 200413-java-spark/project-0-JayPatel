26200a17a1aa3e464db45eb4d0190d85fdf3332f
Jay Patel <6jaypatel@gmail.com>
2020-04-18 19:42:33 -0400

    Added file reading, refactored some code

--- src/main/java/com/github/jaypatel/hangman/Hangman.java
+++ src/main/java/com/github/jaypatel/hangman/Hangman.java
@@ -1,7 +1,6 @@
 package com.github.jaypatel.hangman;
 
 import java.util.Scanner;
-import java.util.ArrayList;
 /**
  * A game of Hangman
  * @author Jay Patel
@@ -9,16 +8,11 @@ import java.util.ArrayList;
  */
 
 public class Hangman {
-    static int strikes = 0;
-    static int strikesAllowed = 15;
-    static String theWord = "glassess";
-    static int counter = 0;
+
     static boolean continueGame = true;
-    static ArrayList<Character> mappedLetters = new ArrayList<Character>();
-    static char[] blanks;
-    static int wrongGuessesCounter = 0;
-    static ArrayList<Character> wrongLetters = new ArrayList<Character>();
     static Scanner scan = new Scanner(System.in);
+    static Word word = new Word();
+
 
     public static void main(String[] args) {
 
@@ -37,9 +31,7 @@ public class Hangman {
                 System.out.println("Thank You");
             }
         }
-        
-
-    }
+    } 
 
     /**
      * Starts the game of Hangman.
@@ -47,131 +39,21 @@ public class Hangman {
     public static void startGame() {
 
         System.out.println("Lets play a game of Hangman");
-        System.out.println("The length of the word is " + theWord.length());
-        createBlanks();
-        printBlanks();
 
+        System.out.println("The length of the word is " + word.theWordLength);
+        word.printBlanks();
         System.out.println("Enter the first letter");
-
         while (continueGame) {
             char letter = scan.next().charAt(0);
-            letter = Character.toLowerCase(letter);
             System.out.println("You entered " + letter);
-
-      
-            String msg = letterEntered(letter);
-
-
+            String msg = word.letterEntered(letter);
             System.out.println(msg);
-            printBlanks();
-            displayWrongLetters();
 
+            word.printBlanks();
+            Word.displayWrongLetters();
+            System.out.println("You have " + (Word.strikesAllowed - Word.strikes) + "/" + Word.strikesAllowed + " wrong guesses left" );
         }
     }
-
-    public static void createBlanks() {
-        blanks = new char[theWord.length()];
-        for (int i = 0; i < blanks.length; i++) {
-            blanks[i] = '-';
-        }
-    }
-
-    public static void printBlanks() {
-        for (int i = 0; i < blanks.length; i++) {
-            System.out.print(blanks[i]);
-        }
-        System.out.println();
-    }
-
-    /**
-     * This method takes in the letter that is entered either verifys if its correct
-     * or not correct returns accordingly
-     * 
-     * @param: The letter that is entered
-     * @return: A string message that says correct/incorrect guess
-     */
-    public static String letterEntered(char letter) {
-        for (int i = 0; i < theWord.length(); i++) {
-            if (letter == blanks[i]) {
-                return "You have already entered this letter";
-
-            }
-        }
-        counter++;
-        if (letter == 0) {
-            counter--;
-            return ("Please enter one character");
-        }
-        if (theWord.indexOf(letter) == -1) {
-            addWrongLetter(letter);
-            strikes++;
-            if (strikes >= strikesAllowed) {
-                continueGame = false;
-                return ("Game Over, You Loose. The word was " + theWord);
-            } else {
-                return "Wrong Guess, Enter next Letter";
-            }
-        } else {
-            for (int i = 0; i < theWord.length(); i++) {
-                if (theWord.charAt(i) == letter) {
-                    mappedLetters.add(letter);
-                    addCorrectLetter(letter, i);
-
-                }
-            }
-
-
-            if (mappedLetters.size() == theWord.length()) {
-                continueGame = false;
-                return "You Win!";
-            } else {
-                for (int i = 0; i < theWord.length(); i++) {
-                    if (theWord.charAt(i) == letter) {
-                        addCorrectLetter(letter, i);
-                    }
-                }
-                
-                return "Correct guess, Enter next Letter.";
-            }
-
-        }
-
-    }
-
-    public static void addWrongLetter(char letter) {
-        wrongLetters.add(letter);
-        wrongGuessesCounter++;
-
-
-    }
-
-    public static void displayWrongLetters() {
-        if (wrongLetters.size() == 0) {
-            return;
-        } else {
-            System.out.print("Wrong Guesses: ");
-
-            for(char i:wrongLetters) {
-                System.out.print(i); 
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
-     
-    }
-
-
-
-    public static void addCorrectLetter(char letter) {
-        int indexOfLetter = theWord.indexOf(letter);
-        blanks[indexOfLetter] = letter;
-    }
-
-    public static void addCorrectLetter(char letter, int index) {
-       
-        blanks[index] = letter;
-    }
-
 
 
 
---
+++ src/main/java/com/github/jaypatel/hangman/Word.java
@@ -0,0 +1,151 @@
+package com.github.jaypatel.hangman;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Word {
+
+     int theWordLength;
+     String theWord;
+     char[] blanks;
+     int counter = 0;
+     static int strikes = 0;
+     static int strikesAllowed = 15;
+     static ArrayList<Character> mappedLetters = new ArrayList<Character>();
+     static ArrayList<Character> wrongLetters = new ArrayList<Character>();
+
+
+     public Word(String theWord) {
+         this.theWord = theWord;
+         this.theWordLength = theWord.length();
+         createBlanks();
+     }
+
+     public Word() {
+         File file = new File(
+                 "/Users/Jay/OneDrive/Desktop/Java/Revature/project-0/src/main/java/com/github/jaypatel/hangman/word.txt");
+         Random r = new Random();
+         int n = 0;
+         try {
+             for (Scanner scan = new Scanner(file); scan.hasNext();) {
+                 n++;
+                 String line = scan.nextLine();
+                 if (r.nextInt(n) == 0) {
+                     theWord = line;
+                 }
+             }
+         } catch (FileNotFoundException e) {
+             e.printStackTrace();
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+         this.theWordLength = theWord.length();
+         createBlanks();
+     }
+
+
+     public void createBlanks() {
+         blanks = new char[theWord.length()];
+         for (int i = 0; i < blanks.length; i++) {
+             if (Character.isLetter(theWord.charAt(i)) || Character.isDigit(theWord.charAt(i))) {
+                 blanks[i] = '-';
+             } else {
+                 blanks[i] = theWord.charAt(i);
+                 theWordLength--;
+             }
+         }
+     }
+
+     public void printBlanks() {
+         for (int i = 0; i < blanks.length; i++) {
+             System.out.print(blanks[i]);
+         }
+         System.out.println();
+     }
+
+
+
+    /**
+     * This method takes in the letter that is entered either verifys if its correct
+     * or not correct returns accordingly
+     * 
+     * @param: The letter that is entered
+     * @return: A string message that says correct/incorrect guess
+     */
+    public  String letterEntered(char letter) {
+        if (!Character.isLetter(letter) && !Character.isDigit(letter)) {
+            return ("Please enter one letter");
+        } 
+        for (int i = 0; i < theWord.length(); i++) {
+            if (letter == blanks[i]) {
+                return "You have already entered this letter";
+            }
+        }
+
+        counter++;
+        if (theWord.indexOf(Character.toLowerCase(letter)) == -1 && (theWord.indexOf(Character.toUpperCase(letter)) == -1)) {
+            addWrongLetter(letter);
+            strikes++;
+            if (strikes >= strikesAllowed) {
+                Hangman.continueGame = false;
+                return ("Game Over, You Loose. The word was '" + theWord +  "'");
+            } else {
+                return "Wrong Guess, Enter next Letter";
+            }
+        } else {
+            for (int i = 0; i < theWord.length(); i++) {
+                if (theWord.charAt(i) == Character.toLowerCase(letter) || theWord.charAt(i) == Character.toUpperCase(letter)) {
+                    mappedLetters.add(letter);
+                    addCorrectLetter(letter, i);
+                }
+            }
+            if (mappedLetters.size() == theWordLength) {
+                Hangman.continueGame = false;
+                return "You Win!";
+            } else {
+                for (int i = 0; i < theWord.length(); i++) {
+                    if (theWord.charAt(i) == letter) {
+                        addCorrectLetter(letter, i);
+                    }
+                }
+                return "Correct guess, Enter next Letter.";
+            }
+        }
+    }
+
+    public static void addWrongLetter(char letter) {
+        wrongLetters.add(letter);
+    }
+
+    public  void addCorrectLetter(char letter) {
+        int indexOfLetter = theWord.indexOf(letter);
+        blanks[indexOfLetter] = letter;
+    }
+
+    public  void addCorrectLetter(char letter, int index) {
+       
+        blanks[index] = letter;
+    }
+
+    public static void displayWrongLetters() {
+        if (wrongLetters.size() == 0) {
+            return;
+        } else {
+            System.out.print("Wrong Guesses: ");
+
+            for(char i:wrongLetters) {
+                System.out.print(i); 
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+     
+    }
+}
+
+
+ 
